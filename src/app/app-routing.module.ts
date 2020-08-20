12f719fa02c2cf9c2c9fe369dbc57cbbf9064345
Marco Faccio <marco.faccio@gmail.com>
2020-08-19 21:00:59 -0300

    subiendo una base del proyecto con routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AppComponent} from './app.component';
+import {GetBooksComponent} from './routing/get-books/get-books.component';
+import {AddBooksComponent} from './routing/add-books/add-books.component';
+import {from} from 'rxjs';
 
-
-const routes: Routes = [];
+const routes: Routes =
+  [
+    {path: '', component: GetBooksComponent}, // index - get book list
+    {path: 'add', component: AddBooksComponent} // add new book
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { GetBooksComponent } from './routing/get-books/get-books.component';
+import { AddBooksComponent } from './routing/add-books/add-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    GetBooksComponent,
+    AddBooksComponent
   ],
   imports: [
     BrowserModule,
