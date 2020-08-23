32695a84f01cff88d5a85325fb526d4ba9f4000d
Marco Faccio <marco.faccio@gmail.com>
2020-08-23 18:20:39 -0300

    Se agregan los imports que necesitamos para trabajar de forma inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { GetBooksComponent } from './routing/get-books/get-books.component';
-import { AddBooksComponent } from './routing/add-books/add-books.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {FooterComponent} from './footer/footer.component';
+import {GetBooksComponent} from './routing/get-books/get-books.component';
+import {AddBooksComponent} from './routing/add-books/add-books.component';
 
 @NgModule({
   declarations: [
@@ -18,9 +20,11 @@ import { AddBooksComponent } from './routing/add-books/add-books.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
